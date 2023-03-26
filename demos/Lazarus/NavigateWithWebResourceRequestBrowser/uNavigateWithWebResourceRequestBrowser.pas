f7b80fe7d03474b2db0ffa98436817fd50e8c0a8
salvadordf <briskbard@briskbard.com>
2023-03-26 16:54:53 +0200

    Updated server in the NavigateWithWebResourceRequestBrowser demo for Lazarus

--- demos/Lazarus/NavigateWithWebResourceRequestBrowser/uNavigateWithWebResourceRequestBrowser.pas
+++ demos/Lazarus/NavigateWithWebResourceRequestBrowser/uNavigateWithWebResourceRequestBrowser.pas
@@ -13,7 +13,11 @@ uses
   uWVBrowserBase;
 
 type
+
+  { TMainForm }
+
   TMainForm = class(TForm)
+    SendCustomHTTPHeaderChk: TCheckBox;
     WVWindowParent1: TWVWindowParent;
     Timer1: TTimer;
     WVBrowser1: TWVBrowser;
@@ -29,7 +33,6 @@ type
     Label4: TLabel;
     AddressCb: TComboBox;
     Label5: TLabel;
-    Button1: TButton;
 
     procedure Timer1Timer(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -39,10 +42,12 @@ type
     procedure WVBrowser1InitializationError(Sender: TObject; aErrorCode: HRESULT; const aErrorMessage: wvstring);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure Button1Click(Sender: TObject);
 
   protected
-    FBody    : TStringStream;
+    FBody    : TStringStream;    
+
+    procedure SendSimpleRequest;
+    procedure SendCustomHeaderRequest;
 
     // It's necessary to handle these messages to call NotifyParentWindowPositionChanged or some page elements will be misaligned.
     procedure WMMove(var aMessage : TWMMove); message WM_MOVE;
@@ -69,10 +74,8 @@ implementation
 // After that you sould see a "Thank you for this dump. I hope you have a lovely day!" message.
 // You can check the results by clicking in the "Check results in PTSV2.com" button.
 
-procedure TMainForm.Button1Click(Sender: TObject);
-begin
-  ShellExecute(0, 'open', 'https://ptsv2.com/t/cef4delphi', nil, nil, SW_SHOWNORMAL);
-end;
+uses
+  uWVCoreWebView2WebResourceRequest, uWVCoreWebView2HttpRequestHeaders;
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
@@ -94,9 +97,9 @@ begin
       WVBrowser1.CreateBrowser(WVWindowParent1.Handle)
      else
       Timer1.Enabled := True;
-end;
+end;            
 
-procedure TMainForm.GoBtnClick(Sender: TObject);
+procedure TMainForm.SendSimpleRequest;
 var
   TempRequest : ICoreWebView2WebResourceRequest;
   TempAdapter : IStream;
@@ -120,6 +123,49 @@ begin
     TempRequest := nil;
     TempAdapter := nil;
   end;
+end;
+
+procedure TMainForm.SendCustomHeaderRequest;
+var
+  TempRequestIntf : ICoreWebView2WebResourceRequest;
+  TempRequest : TCoreWebView2WebResourceRequestRef;
+  TempAdapter : IStream;
+  TempParams, TempMethod : string;
+  TempHeaders  : TCoreWebView2HttpRequestHeaders;
+begin
+  TempRequest := nil;
+  TempAdapter := nil;
+  TempHeaders := nil;
+
+  try
+    if assigned(FBody) then FreeAndNil(FBody);
+
+    TempMethod  := 'POST';
+    TempParams  := PostParam1NameEdt.Text + '=' + PostParam1ValueEdt.Text + '&' + PostParam2NameEdt.Text + '=' + PostParam2ValueEdt.Text;
+    FBody       := TStringStream.Create(TempParams);
+    TempAdapter := TStreamAdapter.Create(FBody, soReference);
+
+    if WVBrowser1.CoreWebView2Environment.CreateWebResourceRequest(AddressCb.Text, TempMethod, TempAdapter, '', TempRequestIntf) then
+      begin
+        TempRequest := TCoreWebView2WebResourceRequestRef.Create(TempRequestIntf);
+        TempHeaders := TCoreWebView2HttpRequestHeaders.Create(TempRequest.Headers);
+        TempHeaders.SetHeader('Content-Type', 'application/x-www-form-urlencoded');
+        TempHeaders.SetHeader('X-MyCustomHeader', 'MyCustomValue');
+        WVBrowser1.NavigateWithWebResourceRequest(TempRequestIntf);
+      end;
+  finally
+    if assigned(TempRequest) then FreeAndNil(TempRequest);
+    if assigned(TempHeaders) then FreeAndNil(TempHeaders);
+    TempAdapter := nil;
+  end;
+end;
+
+procedure TMainForm.GoBtnClick(Sender: TObject);
+begin
+  if SendCustomHTTPHeaderChk.Checked then
+    SendCustomHeaderRequest
+   else
+    SendSimpleRequest;
 end;
 
 procedure TMainForm.WVBrowser1AfterCreated(Sender: TObject);
